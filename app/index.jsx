bfe9ce4855b9d9f4cbc0f43ffe24acb272df8aca
skyfourhmmm <nguyenthientu413.com>
2024-09-25 22:24:36 +0700

    fixing_@

--- app/index.jsx
+++ app/index.jsx
@@ -2,16 +2,12 @@ import { Text, View } from "react-native";
 import MainLayout from "../components/layout/MainLayout";
 import { Provider } from "react-redux";
 import store from "../redux/store";
-import { ButtomTabs } from "../components/common/ButtomTabs";
-import { NavigationContainer } from "@react-navigation/native";
 
 export default function Index() {
   return (
     <Provider store={store}>
       <MainLayout>
-        <NavigationContainer>
-          <ButtomTabs />
-        </NavigationContainer>
+        <Text>Dating app starting...</Text>
       </MainLayout>
     </Provider>
   );
