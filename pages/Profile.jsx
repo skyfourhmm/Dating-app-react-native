01d23685e98d4a01ddc69373e26619c099d1f45c
2henC <nguyenthanh8a2@gmail.com>
2024-10-02 11:30:10 +0700

    complete 50% Profile

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView, Image } from "react-native";
-import { Button, Icon } from "react-native-paper";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  FlatList,
+} from "react-native";
+import { Button, Checkbox, Icon } from "react-native-paper";
 
 const styles = StyleSheet.create({
   container: {
@@ -11,18 +18,83 @@ const styles = StyleSheet.create({
   menu: {
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  header: {},
+  header_button_edit_profile: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#c8f9ff",
+    borderRadius: 50,
+    marginTop: 10,
+  },
+  header_verify: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 20,
+    backgroundColor: "#f8f9fa",
+    padding: 10,
+  },
+  body: {},
+  body_header: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 30,
+    alignItems: "center",
+  },
+  body_header_text: {
+    color: "#00bdd6",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  body_header_active: {
+    borderBottomColor: "#00bdd6",
+    borderBottomWidth: 3,
+    padding: 10,
+  },
+  body_bander: {
+    marginVertical: 20,
+    backgroundColor: "#00bdd6",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
     paddingVertical: 10,
-    // marginBottom: 20,
+    paddingHorizontal: 25,
   },
-  header: {
-    // flexDirection: "row",
-    // justifyContent: "center",
-  },
-  body: {
-    borderWidth: 1,
-    borderColor: "red",
+  body_content_header: {
+    flex: 2,
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
+
+const features = [
+  {
+    id: 1,
+    name: "Unlimited swipes",
+    type: ["Free", "Premium"],
+  },
+  {
+    id: 2,
+    name: "Advanced filters",
+    type: ["Free", "Premium"],
+  },
+  {
+    id: 3,
+    name: "Remove ads",
+    type: ["Premium"],
+  },
+  {
+    id: 4,
+    name: "Undo accidental left swipes",
+    type: ["Premium"],
+  },
+  {
+    id: 5,
+    name: "Push you profile to more viewers",
+    type: ["Premium"],
+  },
+];
 
 function Profile() {
   return (
@@ -53,15 +125,7 @@ function Profile() {
               </Text>
               <Icon source="shield-check" color="gray" size={12} />
             </View>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                backgroundColor: "#c8f9ff",
-                borderRadius: 50,
-                marginTop: 10,
-              }}
-            >
+            <View style={styles.header_button_edit_profile}>
               <Button onPress={() => alert("Pressed")} textColor="#00bdd6">
                 Edit your profile
               </Button>
@@ -70,15 +134,7 @@ function Profile() {
           </View>
         </View>
 
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            marginTop: 20,
-            backgroundColor: "#f8f9fa",
-            padding: 10,
-          }}
-        >
+        <View style={styles.header_verify}>
           <View
             style={{
               backgroundColor: "#e7f1fa",
@@ -107,7 +163,68 @@ function Profile() {
       </View>
 
       <View style={styles.body}>
-        <Text>Body</Text>
+        <View style={styles.body_header}>
+          <View style={styles.body_header_active}>
+            <Text style={styles.body_header_text}>Plans</Text>
+          </View>
+          <View>
+            <Text style={styles.body_header_text}>Safety</Text>
+          </View>
+        </View>
+
+        <View style={styles.body_bander}>
+          <Text style={{ color: "white", fontWeight: "bold", fontSize: 18 }}>
+            HeartSync Premium
+          </Text>
+          <Text style={{ color: "white", fontSize: 13, textAlign: "center" }}>
+            Unlock exclusive features and supercharge your dating experience
+          </Text>
+          <Button mode="contained" buttonColor="#ffffff" textColor="gray">
+            Upgrade from $7.99
+          </Button>
+        </View>
+
+        <View style={styles.body_content}>
+          <View>
+            <View style={{ flexDirection: "row" }}>
+              <Text style={{ fontWeight: "bold", flex: 4 }}>
+                What's included
+              </Text>
+              <Text style={styles.body_content_header}>Free</Text>
+              <Text style={[styles.body_content_header, { color: "#00bdd6" }]}>
+                Premium
+              </Text>
+            </View>
+          </View>
+          <View>
+            <FlatList
+              data={features}
+              renderItem={({ item }) => (
+                <View style={{ flexDirection: "row" }}>
+                  <View style={{ flex: 4 }}>
+                    <Text style={{ marginVertical: 5 }}>{item.name}</Text>
+                  </View>
+                  <View style={{ flex: 2, alignItems: "center" }}>
+                    <Checkbox
+                      disabled={true}
+                      status={
+                        item.type.includes("Free") ? "checked" : "unchecked"
+                      }
+                      color="#00bdd6"
+                    />
+                  </View>
+                  <View style={{ flex: 2, alignItems: "center" }}>
+                    <Checkbox
+                      disabled={true}
+                      status="checked"
+                      color="#00bdd6"
+                    />
+                  </View>
+                </View>
+              )}
+            />
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
